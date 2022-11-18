0f4a974a5841553c0d4b5ab97d5a0af8630e8405
Karim Saad <Karim.Saad@macbooks-MacBook-Pro-2.local>
2022-11-18 20:54:42 +0200

    installing ios

--- ios/MazadatImagePicker3.swift
+++ ios/MazadatImagePicker3.swift
@@ -3,6 +3,8 @@ class MazadatImagePicker3: NSObject {
 
   @objc(multiply:withB:withResolver:withRejecter:)
   func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+      
+      let camera=CameraController(nibName: "CameraController", bundle: nil)
     resolve(a*b)
   }
 }
